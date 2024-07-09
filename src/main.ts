4a0203e6e42f8a738906ccef725e0e5b1ea03c0e
nfuegopy <acbleguizamon@gmail.com>
2024-07-09 16:47:01 -0400

    Se configura el CORS para recibir peticion de cualquier URL

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // app.enableCors({
+  //   origin: 'http://localhost:8080',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
   app.enableCors({
-    origin: 'http://localhost:8080',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
   });
-
+  
   await app.listen(3000);
 }
 bootstrap();
