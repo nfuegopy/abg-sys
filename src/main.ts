e3869b7625f26ad75e3dda49b3a0d6adf92ad7e0
Antonio Barrios <acbleguizamon@gmail.com>
2024-07-09 21:31:31 -0400

    Se agrega los key y puertos de acuerdo a render

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app/app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+  
+//   // app.enableCors({
+//   //   origin: 'http://localhost:8080',
+//   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//   //   credentials: true,
+//   // });
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//     credentials: true,
+//     allowedHeaders: 'Content-Type, Accept',
+//   });
+  
+//   await app.listen(3000);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // app.enableCors({
-  //   origin: 'http://localhost:8080',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -18,6 +36,8 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
   });
   
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
