4a970a9430cb977e5a0b0fc889f2b881b933f68f
Antonio Barrios <acbleguizamon@gmail.com>
2024-07-09 21:11:49 -0400

    Modificacion Firebase admin para tomar la variable del entorno

--- src/firebase-admin/firebase-admin.module.ts
+++ src/firebase-admin/firebase-admin.module.ts
@@ -1,8 +1,47 @@
+// import { Module, Global } from '@nestjs/common';
+// import * as admin from 'firebase-admin';
+// import { ConfigService } from '@nestjs/config';
+// import * as fs from 'fs';
+// import * as path from 'path';
+
+// @Global()
+// @Module({
+//   providers: [
+//     {
+//       provide: 'FIREBASE_ADMIN',
+//       useFactory: (configService: ConfigService) => {
+//         let serviceAccountPath = configService.get<string>('FIREBASE_SERVICE_ACCOUNT_KEY');
+//         console.log('Service account path from env:', serviceAccountPath);
+
+//         if (!serviceAccountPath) {
+//           serviceAccountPath = path.join(__dirname, '..', '..', 'serviceAccountKey.json');
+//           console.log('Fallback service account path:', serviceAccountPath);
+//         }
+
+//         if (!fs.existsSync(serviceAccountPath)) {
+//           throw new Error(`Service account key file not found at path: ${serviceAccountPath}`);
+//         }
+
+//         try {
+//           const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+//           return admin.initializeApp({
+//             credential: admin.credential.cert(serviceAccount),
+//           });
+//         } catch (error) {
+//           console.error('Error initializing Firebase Admin:', error);
+//           throw error;
+//         }
+//       },
+//       inject: [ConfigService],
+//     },
+//   ],
+//   exports: ['FIREBASE_ADMIN'],
+// })
+// export class FirebaseAdminModule {}
+
 import { Module, Global } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { ConfigService } from '@nestjs/config';
-import * as fs from 'fs';
-import * as path from 'path';
 
 @Global()
 @Module({
@@ -10,20 +49,13 @@ import * as path from 'path';
     {
       provide: 'FIREBASE_ADMIN',
       useFactory: (configService: ConfigService) => {
-        let serviceAccountPath = configService.get<string>('FIREBASE_SERVICE_ACCOUNT_KEY');
-        console.log('Service account path from env:', serviceAccountPath);
-
-        if (!serviceAccountPath) {
-          serviceAccountPath = path.join(__dirname, '..', '..', 'serviceAccountKey.json');
-          console.log('Fallback service account path:', serviceAccountPath);
-        }
-
-        if (!fs.existsSync(serviceAccountPath)) {
-          throw new Error(`Service account key file not found at path: ${serviceAccountPath}`);
+        const firebaseCredentials = configService.get<string>('FIREBASE_CREDENTIALS');
+        if (!firebaseCredentials) {
+          throw new Error(`Firebase credentials are not defined in the environment variables.`);
         }
 
         try {
-          const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+          const serviceAccount = JSON.parse(firebaseCredentials);
           return admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
           });
