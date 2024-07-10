b01e540efb19fc4ed57d7280a67f7af750c77edd
Antonio Barrios <acbleguizamon@gmail.com>
2024-07-09 20:43:34 -0400

    Deploy corregido rutas de case-history para subir a render

--- src/modules/movimientos/CaseHistory/module/case-history.module.ts
+++ src/modules/movimientos/CaseHistory/module/case-history.module.ts
@@ -1,7 +1,7 @@
 // src/modules/Movimientos/CaseHistory/Module/case-history.module.ts
 import { Module } from '@nestjs/common';
-import { CaseHistoryService } from '../Service/case-history.service';
-import { CaseHistoryController } from '../Controller/case-history.controller';
+import { CaseHistoryService } from '../service/case-history.service';
+import { CaseHistoryController } from '../controller/case-history.controller';
 
 @Module({
   providers: [CaseHistoryService],
